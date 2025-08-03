8b46d447bdaa47c5309d29498355453f1ad24506
Soumyajit <soumyajitparia2005@gmail.com>
2025-08-03 22:41:17 +0530

    Implement user login and logout functionality with JWT verification

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,13 +4,28 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
-    if(!token){
-        throw new ApiError(401, "Unauthorized request");
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
     }
 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    await User.findById(decodedToken?._id)
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
 
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+      throw new ApiError(401, error?.message || "Invalid access token");
+  }
 });
